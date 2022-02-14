e14efc12b0d3e8a073a74717c5d1209945800a86
shreyasicodes20 <97606334+shreyasicodes20@users.noreply.github.com>
2022-02-14 11:12:03 +0530

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,15 @@
+#functions
+def add_numbers(a,b): #def-> Keyword, a,b-> arguments
+ c=a+b
+ return c
+ 
+def subtract(a,b):
+ c=a-b
+ return c
+ 
+#-----------Main Program-------------------
+ 
+x=2
+y=9
+print(add_numbers(1,3))
+print(subtract(8,2))
